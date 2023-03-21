0dc5755d8a2d45042f4e8c70e34b6dae642f099d
lakca <912910011@qq.com>
2023-03-21 18:04:05 +0800

    update image script.

--- shell/image.py
+++ shell/image.py
@@ -1,14 +1,21 @@
 #! /usr/bin/env python3
 
 from sys import argv
-from PIL import Image, ImageGrab
-from os import path
+import time
+from PIL import Image, ImageGrab, ImageEnhance
+from os import path, getcwd
 from base64 import b64encode, b64decode
 import subprocess
 import io
 
 op = argv[1]
 args = argv[2:]
+
+def check_filename(filename):
+    if path.isfile(filename):
+        name, ext = path.splitext(filename)
+        return name + '.' + str(int(time.time())) + ext
+    return filename
 
 if '-h' in argv:
     print("""
@@ -21,7 +28,8 @@ if '-h' in argv:
         paste <file>
 
         \033[2m# 转换图片格式\033[0m
-        to <ext> [at <destination>] <...image files>
+        \033[2m# mode: enhance soften brighten darken sharpen blur\033[0m
+        to <[ext|mode...]> [at <destination>] <...image files>
 
         \033[2m# （从剪切板或文件）获取图片base64\033[0m
         base64 [file]
@@ -46,22 +54,62 @@ elif op == 'shot':
 
 elif op == 'to':
     files = []
-    ext = args.pop(0)
-    ext = '.' + ext if not ext.startswith('.') else ext
-    dest = ''
+    modes = ['']
+    ext = ''
+    dest = getcwd()
+    force = False
+    for handle in args.pop(0).split(','):
+        if handle.startswith('.'):
+            ext = handle
+        else:
+            modes.append(handle)
     while args:
         arg = args.pop(0)
         if arg == 'at':
             dest = path.realpath(args.pop(0))
+        if arg == '!':
+            force = True
+        elif arg == '--':
+            while args:
+                files.append(path.abspath(args.pop(0)))
         else:
             files.append(path.abspath(arg))
-    for file in files:
-        folder = dest or path.dirname(file)
-        filename = path.basename(file).rpartition('.')[0]
-        img = Image.open(file)  # 创建图像实例
-        dest = path.join(folder, filename + ext)
-        img.save(dest)
-        print('保存', dest)
+
+    filename = ''
+    if path.isfile(dest) or not path.isdir(dest):
+        filename = dest
+        if ext and filename.endswith(ext):
+            filename = filename.removesuffix(ext)
+
+    for i, file in enumerate(files):
+        to = filename + (f'-{i}' if i > 0 else '') + ext if filename else path.join(dest, path.splitext(path.basename(file))[0] + ext if ext else path.basename(file))
+
+        if not force:
+            to = check_filename(to)
+
+        img = Image.open(file)
+        if modes:
+            for mode in modes:
+                params = mode.split(',')
+                mode = params.pop(0)
+                if mode == 'gray':
+                    img = img.convert(mode='L')
+                elif mode == 'enhance':
+                    img = ImageEnhance.Contrast(img).enhance(float(params[0]) if params else 1.3)
+                elif mode == 'soften':
+                    img = ImageEnhance.Contrast(img).enhance(float(params[0]) if params else 0.7)
+                elif mode == 'brighten':
+                    img = ImageEnhance.Brightness(img).enhance(float(params[0]) if params else 1.3)
+                elif mode == 'darken':
+                    img = ImageEnhance.Brightness(img).enhance(float(params[0]) if params else 0.7)
+                elif mode == 'sharpen':
+                    img = ImageEnhance.Sharpness(img).enhance(float(params[0]) if params else 1.3)
+                elif mode == 'blur':
+                    img = ImageEnhance.Sharpness(img).enhance(float(params[0]) if params else 0.7)
+                else:
+                    print(f'未知模式: {mode}')
+        img.save(to)
+        print('保存', f'\033[31m{to}\033[0m')
 
 elif op == 'base64':
     file = args.pop(0) if args else None
@@ -72,4 +120,3 @@ elif op == 'base64':
         img.save(buffered, format=img.format)
         text = f"data:image/{img.format.lower()};base64," + str(b64encode(buffered.getvalue()), 'utf8')
         subprocess.Popen('pbcopy', stdin=subprocess.PIPE).communicate(input=text.encode())
-
