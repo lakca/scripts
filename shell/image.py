b3f34685d2afd9354dbd3273a52252928a0d28dd
lakca <912910011@qq.com>
2023-03-24 18:18:08 +0800

    update image script.

--- shell/image.py
+++ shell/image.py
@@ -91,7 +91,7 @@ if op == 'paste':
     img = ImageGrab.grabclipboard()
     img.save(file)
     if args and 'open' in args:
-        f"open {file}"
+        subprocess.Popen(f"open '{file}'")
     print('保存', file)
 
 elif op == 'shot':
